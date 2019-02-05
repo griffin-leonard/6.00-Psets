001976c56fb9e7f494af303a676d60075af3823f
Griffin Leonard <47362102+griffin-leonard@users.noreply.github.com>
2019-02-05 17:17:01 -0500

    Add files via upload

---
+++ 6-0001_ps0.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Problem Set 0
+Name: Griffin Leonard
+Collaborators: n/a
+Time Spent: 0:30
+"""
+
+import numpy
+x = int(input("Enter a x value: "))
+y = int(input("Enter a y value: "))
+print()
+print("x^y =", x**y)
+print("log2(x) =", numpy.log2(x))
